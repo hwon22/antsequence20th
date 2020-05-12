60b04dc85e6b9c410f59501dcc411e776c2b4b3e
hwon22 <s2018s06@e-mirim.hs.kr>
2020-05-12 22:17:46 +0900

    개미수열 실습

--- antsequence20th/Program.cs
+++ antsequence20th/Program.cs
@@ -8,8 +8,47 @@ namespace antsequence20th
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(String[] args)
         {
+            int select = 20;
+            int count = 1;
+            int a = 0;
+            int b = 0;
+
+            int[] array1 = new int[500];
+            int[] array2 = new int[500];
+
+            array1[0] = 1;
+
+            for (int i = 0; i < select; i++)
+            {
+                while (array1[a] != 0)
+                { 
+                    if (array1[a] == array1[a + 1])
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        array2[b] = array1[a];       
+                        array2[b + 1] = count;      
+                        count = 1;                  
+                        b = b + 2;                  
+                    }
+                    a++;
+                }
+                
+                Array.Copy(array2, array1, array2.Length);
+                a = 0;
+                b = 0;
+                Console.Write(i + 1 + "번째: ");
+                foreach (var arr in array1)
+                {
+                    if (arr == 0) break;
+                    Console.Write(arr);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
